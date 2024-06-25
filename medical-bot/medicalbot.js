1a0ed44b5a66847d5a6d18595e4100f19481444b
HarshwardhanJain <harshwardhan11225@gmail.com>
2024-06-25 12:41:37 +0530

    Updated medicalbot.js And index.js - Working Using The Prediction URLs And Keys

--- medical-bot/medicalbot.js
+++ medical-bot/medicalbot.js
@@ -1,27 +1,45 @@
 const { ActivityHandler, MessageFactory } = require('botbuilder');
+const axios = require('axios');
 
 class MEDICALBOT extends ActivityHandler {
     constructor() {
         super();
-        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
-            // By calling next() you ensure that the next BotHandler is run.
+            const question = context.activity.text;
+            const answer = await this.getAnswerFromQnAService(question);
+            await context.sendActivity(MessageFactory.text(answer, answer));
             await next();
         });
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome!';
+            const welcomeText = 'Hello and welcome! I can help with medical questions.';
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
                     await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
                 }
             }
-            // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
+    }
+
+    async getAnswerFromQnAService(question) {
+        const response = await axios.post(process.env.QNA_ENDPOINT, {
+            top: 1,
+            question: question,
+            includeUnstructuredSources: true
+        }, {
+            headers: {
+                'Ocp-Apim-Subscription-Key': process.env.QNA_KEY,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.data.answers && response.data.answers.length > 0) {
+            return response.data.answers[0].answer;
+        } else {
+            return 'I am not sure about that. Can you please provide more details?';
+        }
     }
 }
 
